47994ef52ce8853845d92f6193a54e234316436f
Yogesh Kumar Singh <55105418+mohityogesh44@users.noreply.github.com>
2022-04-15 00:40:07 +0530

    Update main.py

--- main.py
+++ main.py
@@ -13,36 +13,17 @@ def load_model():
 
 if __name__=="__main__":
 
-    #Load Model
     with st.spinner('Model is being loaded..'):
-        model=load_model()
+            model=load_model()
 
     st.title("Object Detector")
     run = st.checkbox('Camera')
-    image = st.checkbox("Image File Upload")
-    
-    #webcam feed
-    if run:
-        st.title("Webcam Feed")
-        FRAME_WINDOW = st.image([])
-        cam = cv2.VideoCapture(0)
-        while run:
-            ret, frame = cam.read()
-            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-            frame = predict(model, frame)
-            FRAME_WINDOW.image(frame)
-        else:
-            cam.release()
-            st.write('Stopped')
 
-    #Image file upload
-    elif image:
-        st.title("Upload Image File")
-        uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
-        if uploaded_file is not None:
-            image = np.array(Image.open(uploaded_file))
-            st.image(image, caption='Uploaded Image.', use_column_width=True)
-            st.write("")
-            st.write("Classifying...")
-            im = predict(model, image)
-            st.image(im)
+    class VideoTransformer(VideoTransformerBase):
+        def transform(self, frame):
+            img = frame.to_ndarray(format="bgr24")
+            img = predict(model, img)
+
+            return img
+    if run:
+        webrtc_streamer(key="objectDetector", video_transformer_factory=VideoTransformer)
