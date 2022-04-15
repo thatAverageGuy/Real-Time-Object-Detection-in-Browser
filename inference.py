bf4c815657a80cbdfe88ee4786d269f6096f7083
Yogesh Kumar Singh <55105418+mohityogesh44@users.noreply.github.com>
2022-04-15 15:31:14 +0530

    Update inference.py

--- inference.py
+++ inference.py
@@ -6,7 +6,8 @@ def predict(model, frame):
     resx = model(frame, size = 416).crop(save=False)
     for d in resx:
         box = list(map(int, list(map(np.round, d['box']))))
+        label = d['label']
         cv2.rectangle(frame, (box[0], box[1]), (box[2], box[3]), (255, 0, 0), 3)
         px, py = box[0], box[1]-10
-        cv2.putText(frame, "Connector", (px, py), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
+        cv2.putText(frame, label, (px, py), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
     return frame
